ca0ebe68e16ae2e8b857cc043e02870cc54a4b5d
Subodh1911 <76460142+Subodh1911@users.noreply.github.com>
2022-01-05 06:48:02 +0530

    tenth done!

---
+++ SqaresInNNChessboard.cpp
@@ -0,0 +1,31 @@
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long n) {
+        // code here
+        if(n==1) { return 1; }
+        long long sum=(n*(n+1)*(2*n+1))/6;
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
