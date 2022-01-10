57e222e960f4ae1c5880e999b64179e734b5895a
Subodh Kumar <subodhkumar2285@gmail.com>
2022-01-10 20:53:09 +0530

    fourteen done!

---
+++ comp 2/BurningTree.cpp
@@ -0,0 +1,117 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+Node *buildTree(string str) {
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    Node *root = new Node(stoi(ip[0]));
+
+    queue<Node *> queue;
+    queue.push(root);
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+        Node *currNode = queue.front();
+        queue.pop();
+        string currVal = ip[i];
+        if (currVal != "N") {
+            currNode->left = new Node(stoi(currVal));
+            queue.push(currNode->left);
+        }
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+        if (currVal != "N") {
+            currNode->right = new Node(stoi(currVal));
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+   int ans=0;
+   int diamete(Node* root,int t) {
+      if(root==NULL) return 0;
+      int x=diamete(root->left,t),y=diamete(root->right,t);
+        if(root->data==t)
+        {
+           ans=max(x,y);
+           return -1;
+        }
+        if(x<0) { x=abs(x);
+           ans=max(ans,x+y);
+           return -1-x;
+        }
+        if(y<0) { y=abs(y);
+           ans=max(ans,x+y);
+           return -1-y;
+        }
+        if(x<y) return(y+1);
+        else return x+1;
+  }
+
+
+    int minTime(Node* root, int target) 
+    {
+        // Your code goes here
+          diamete(root,target);
+          return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) 
+    {    
+        string treeString;
+        getline(cin, treeString);
+        // cout<<treeString<<"\n";
+        int target;
+        cin>>target;
+        // cout<<target<<"\n";
+
+        Node *root = buildTree(treeString);
+        Solution obj;
+        cout<<obj.minTime(root, target)<<"\n"; 
+
+        cin.ignore();
+
+    }
+
+
+    return 0;
+}
+  // } Driver Code Ends
